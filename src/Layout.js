78f871312c99292e688aae975c58804f269b59b0
Mike James <m.l.james@me.com>
2016-06-08 18:37:18 +0100

    Layout stateless component running via flow

--- src/Layout.js
+++ src/Layout.js
@@ -1,6 +1,7 @@
+// @flow
 import React from 'react';
 
-export default function Layout({ children }) {
+export default function Layout({ children }: {children?: any}) : React.Element {
   return (
     <div>
       <h1>Hello, world!</h1>
